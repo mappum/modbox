477e42d624565b05b5887166fa62cda1fc525543
Matt Bell <mappum@gmail.com>
2019-04-04 16:41:01 -0700

    Added a TODO for wrapping built-in functions with burn call in prelude

--- src/prelude.js
+++ src/prelude.js
@@ -1,8 +1,12 @@
 'use strict'
 
-function prelude () {
+function prelude (burnId) {
   // TODO: protect against Function/etc constructors
   //       so that an attacker can't run uninstrumented code
+
+  // TODO: wrap all built-in functions to make a `burn` call
+  //       so an attacker can't do:
+  //         `bigArray.forEach(someBuiltInFunction)`
 
   let module = { exports: {} }
 
